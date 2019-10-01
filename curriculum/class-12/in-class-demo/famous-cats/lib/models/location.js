d33b6aee60458d2b9e2b221ea346a083239ba58d
martypdx <marty@martypdx.com>
2019-10-01 11:38:30 -0700

    use custom model static methods to have detailed model update info

--- curriculum/class-12/in-class-demo/famous-cats/lib/models/location.js
+++ curriculum/class-12/in-class-demo/famous-cats/lib/models/location.js
@@ -18,4 +18,43 @@ const schema = new Schema({
   }]
 });
 
+// schema.static('addShow', function(id, show) {
+//   //...
+// });
+
+schema.statics = {
+  addShow(id, show) {
+    return this.updateById(
+      id,
+      {
+        $push: {
+          shows: show
+        }
+      }
+    )
+      .then(location => location.shows);
+  },
+
+  removeShow(id, showId) {
+    return this.updateById(id, {
+      $pull: {
+        shows: { _id: showId }
+      }
+    })
+      .then(location => location.shows);
+  },
+
+  updateShowName(id, showId, name) {
+    return this.updateOne(
+      { _id: id, 'shows._id': showId },
+      {
+        $set: {
+          'shows.$.name': name
+        }
+      }
+    )
+      .then(location => location.shows);
+  }
+};
+
 module.exports = mongoose.model('Location', schema);
--- curriculum/class-12/in-class-demo/famous-cats/lib/routes/locations.js
+++ curriculum/class-12/in-class-demo/famous-cats/lib/routes/locations.js
@@ -17,57 +17,27 @@ router
       .catch(next);
   })
 
-  .put('/:id', addGeo(), (req, res, next) => {
-    Location.updateById(
-      req.params.id,
-      req.body
-    )
+  .put('/:id', addGeo(), ({ params, body }, res, next) => {
+    Location.updateById(params.id, body)
       .then(location => res.json(location))
       .catch(next);
   })
 
-  .post('/:id/shows', (req, res, next) => {
-    Location.updateById(
-      req.params.id,
-      {
-        $push: {
-          shows: req.body
-        }
-      }
-    )
-      .then(location => {
-        res.json(location.shows[location.shows.length - 1]);
-      })
+  .post('/:id/shows', ({ params, body }, res, next) => {
+    Location.addShow(params.id, body)
+      .then(shows => res.json(shows))
       .catch(next);
   })
 
-  .delete('/:id/shows/:showId', (req, res, next) => {
-    Location.updateById(
-      req.params.id,
-      {
-        $pull: {
-          shows: { _id: req.params.showId }
-        }
-      }
-    )
-      .then(location => {
-        res.json(location.shows);
-      })
+  .delete('/:id/shows/:showId', ({ params }, res, next) => {
+    Location.removeShow(params.id, params.showId)
+      .then(shows => res.json(shows))
       .catch(next);
   })
 
-  .put('/:id/shows/:showId/name', (req, res, next) => {
-    Location.updateOne(
-      { _id: req.params.id, 'shows._id': req.params.showId },
-      {
-        $set: {
-          'shows.$.name': req.body.name
-        }
-      }
-    )
-      .then(location => {
-        res.json(location.shows);
-      })
+  .put('/:id/shows/:showId/name', ({ params, body }, res, next) => {
+    Location.updateShowName(params.id, params.showId, body.name)
+      .then(shows => res.json(shows))
       .catch(next);
   });
 
